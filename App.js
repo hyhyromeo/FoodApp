357fdb2da0868ea88bd6ae4805e63fd846fea4b7
Brian NG <brian@ggdudes.com>
2022-07-03 17:45:31 +0800

    Again

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/screen/Home';
+import Map from './src/screen/Map';
+import Tabs from './src/nav/Tabs';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    //   <Tabs/>
+    // </NavigationContainer>
+    <Home/>
   );
 }
 
